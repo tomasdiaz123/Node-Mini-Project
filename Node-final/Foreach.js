f1c6664509da544f9cc1b591e8202b36ea4d3b96
tomasdiaz123 <57632207+tomasdiaz123@users.noreply.github.com>
2022-05-17 11:15:03 +0100

    Delete Node-final directory

--- Node-final/Foreach.js
+++
@@ -1,6 +0,0 @@
-Array.prototype.asyncForEach = async function (myFunction) {
-    for (i = 0; i < this.length; i++) {
-        await myFunction(this[i])
-    }
-}
-/*(textoS)=>{console.log("<h2>"+{texto}+"</h2>")*/
